32e2d2211e2da77b7909e0ffc25480be03917c4e
youraverni <worldocuments@gmail.com>
2024-10-02 10:43:49 +0300

    Add files via upload

---
+++ Базовые структуры данных дз.py
@@ -0,0 +1,14 @@
+#1st program
+print((9**0.5)*5)
+#2nd programm
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+#4th program
+print('123.456')# вывести цифру 4( первая после запятой)
+print(123.456*10)
+print(int(1234.56))
+print(1234/10)
+print(123.4%1)
