cea40b9717b8f805b1a0502c4d41f388f209343a
youraverni <worldocuments@gmail.com>
2024-10-10 10:40:33 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+# my_dict = {"Ali": 2000, "Denis": 2001, "Zarina": 2002, "Alice": 2003 }
+# print(my_dict)
+# print(my_dict["Alice"])
+# print(my_dict.get("Qwartet"))
+# my_dict.update({"Nastya":2004,
+#                 "Sasha":2005})
+# print(my_dict)
+# print(my_dict.pop("Denis"))
+# print(my_dict)
+
+
+my_set = {1,1,1,1,2,2,2,2,4,4,5,6,7,True, "String", (1,2,3,4,5,6)}
+my_set.add(777)
+my_set.add(77)
+my_set.remove(1)
+print(my_set)
